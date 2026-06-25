a07a6eab2e8f1d16aaf0b5ee538ff12d7d71e8d7
Iuliia Nikolskaia <nikolskaia@uochb.cas.cz>
2026-06-25 13:36:55 +0200

    Update NoveltyDataset
    add getitem for batch to store novelty label
    add identifier to the data
    remove unnecessary imports

--- massspecgymnovelty/data/novelty_dataset.py
+++ massspecgymnovelty/data/novelty_dataset.py
@@ -1,19 +1,6 @@
 import pandas as pd
-import json
-import os
-import typing as T
-import numpy as np
 import torch
-import matchms
-from pathlib import Path
-from torch.utils.data.dataset import Dataset
-from torch.utils.data.dataloader import default_collate
-from torch_geometric.data import Data, Batch
-from matchms.importing import load_from_mgf
-
-import massspecgym.utils as utils
-from massspecgym.data.transforms import SpecTransform, MolTransform, MolToInChIKey, MetaTransform
-from massspecgym.simulation_utils.misc_utils import flatten_lol
+from massspecgym.data.datasets import MassSpecDataset
 
 class NoveltyDataset(MassSpecDataset):
     """
@@ -21,28 +8,41 @@ class NoveltyDataset(MassSpecDataset):
     """
 
     def __init__(
-            self, 
-            split_with_labels_pth: str = '../../cluster_split.tsv'
-            **kwargs,
+        self,
+        split_with_labels_pth: str = '../../cluster_split.tsv',
+        **kwargs,
     ):
-        super().__init__(**kwargs)
+        self.label_col = "novel_label"
         self.split_with_labels_pth = split_with_labels_pth
-    
-    
+        super().__init__(**kwargs)
+
+
     def load_data(self):
         super().load_data()
         split_with_labels = pd.read_csv(self.split_with_labels_pth, sep='\t')
 
         if "inchikey" not in self.metadata.columns:
-              self.metadata["inchikey"] = self.metadata["smiles"].apply(utils.smiles_to_inchi_key)
+            self.metadata["inchikey"] = self.metadata["smiles"].apply(utils.smiles_to_inchi_key)
 
-        key_to_fold = split_with_labels.set_index("inchikey")["fold"]
+        indexed = split_with_labels.set_index("inchikey")
+        key_to_fold = indexed["fold"]
         self.metadata["fold"] = self.metadata["inchikey"].map(key_to_fold)
+
+        self.metadata["novelty_label"] = self.metadata["inchikey"].map(indexed[self.label_col]).astype(float)
 
         mask = self.metadata["fold"].notna()
         if not mask.all():
-                print(f"[NoveltyDataset] dropping {(~mask).sum()} spectra not covered by the split")
-                self.metadata = self.metadata[mask]
-                self.spectra  = self.spectra[self.metadata.index].reset_index(drop=True)
-                self.metadata = self.metadata.reset_index(drop=True)
+            print(f"[NoveltyDataset] dropping {(~mask).sum()} spectra not covered by the split")
+            self.metadata = self.metadata[mask]
+            self.spectra  = self.spectra[self.metadata.index].reset_index(drop=True)
+            self.metadata = self.metadata.reset_index(drop=True)
 
+        if "identifier" in self.metadata.columns:
+            for spec, identifier in zip(self.spectra, self.metadata["identifier"]):
+                spec.set("identifier", identifier)
+
+    def __getitem__(self, i) -> dict:
+        item = super().__getitem__(i)
+        metadata = self.metadata.iloc[i]
+        item["labels"] = torch.as_tensor(metadata["novelty_label"], dtype=self.dtype)
+        return item
